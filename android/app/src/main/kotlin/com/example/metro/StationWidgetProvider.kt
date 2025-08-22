b9f2aa1eb24cd2f2344e4ede890cd81d9ed3d6f4
RayminQAQ <raymin0425@gmail.com>
2025-08-22 15:55:21 +0800

    Update Widget: station widget

---
+++ android/app/src/main/kotlin/com/example/metro/StationWidgetProvider.kt
@@ -0,0 +1,14 @@
+package com.example.metro
+
+import android.app.AlarmManager
+import android.app.PendingIntent
+import android.appwidget.AppWidgetManager
+import android.appwidget.AppWidgetProvider
+import android.content.ComponentName
+import android.content.Context
+import android.content.Intent
+import android.widget.RemoteViews
+
+class StationWidgetProvider : AppWidgetProvider() {
+
+}
